adb4232249a29f42596286056909cf2797961b6c
Ironwilly <claseswilly@gmail.com>
2021-05-12 11:39:26 +0200

    Clase 12 mayo 2021

---
+++ TrianaAdvisor/src/main/java/com/salesianostriana/dam/repo/BarRepository.java
@@ -0,0 +1,11 @@
+package com.salesianostriana.dam.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.salesianostriana.dam.modelo.Bar;
+
+public interface BarRepository extends JpaRepository<Bar, Long> {
+	
+	
+
+}
---
+++ TrianaAdvisor/src/main/java/com/salesianostriana/dam/servicio/BarServicio.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.servicio;
+
+import org.springframework.boot.liquibase.LiquibaseServiceLocatorApplicationListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BarServicio extends BarService
+
+}
