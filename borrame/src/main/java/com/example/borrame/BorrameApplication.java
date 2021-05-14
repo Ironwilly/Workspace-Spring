99c9c4960c9e0c89036ceca7e16a38c413999c95
Ironwilly <claseswilly@gmail.com>
2021-05-14 14:28:29 +0200

    Clase 14 mayo 2021

---
+++ borrame/src/main/java/com/example/borrame/BorrameApplication.java
@@ -0,0 +1,13 @@
+package com.example.borrame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BorrameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BorrameApplication.class, args);
+	}
+
+}
